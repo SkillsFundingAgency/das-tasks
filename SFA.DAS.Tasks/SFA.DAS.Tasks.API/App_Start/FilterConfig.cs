341c45080e37f25992f012ef5f88db33bdc2e50e
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-08-01 11:49:26 +0100

    Added basic cloud service to project
    - Also added skeleton Web API project for API
    - Also added worker project for Tasks worker

---
+++ SFA.DAS.Tasks/SFA.DAS.Tasks.API/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SFA.DAS.Tasks.API
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SFA.DAS.Tasks/SFA.DAS.Tasks.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SFA.DAS.Tasks.API.WebApiApplication" Language="C#" %>
