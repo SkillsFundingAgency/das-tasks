09f3fd06fdff91f4ea2b9c2f9ff40aac2a8cac92
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-08-04 15:12:26 +0100

    Added API types project with new DTO

---
+++ src/SFA.DAS.Tasks.API.Types/DTOs/TaskDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Tasks.API.Types.DTOs
+{
+    public class TaskDto
+    {
+        public string Type { get; set; }
+        public string OwnerId { get; set; }
+        public int ItemsDueCount { get; set; }
+    }
+}
