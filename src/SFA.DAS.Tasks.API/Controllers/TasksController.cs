a68b0ef5a601d9041981954d572eac14cfae5751
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-08-10 09:43:53 +0100

    Added mediator code

--- src/SFA.DAS.Tasks.API/Controllers/TasksController.cs
+++ src/SFA.DAS.Tasks.API/Controllers/TasksController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Web.Http;
+using MediatR;
 
 
 namespace SFA.DAS.Tasks.API.Controllers
@@ -8,6 +9,13 @@ namespace SFA.DAS.Tasks.API.Controllers
     [RoutePrefix("api/tasks/{ownerId}")]
     public class TasksController : ApiController
     {
+        private readonly IMediator _mediator;
+
+        public TasksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
         [Route("")]
         public async Task<IHttpActionResult> GetTasks(string ownerId)
         {
