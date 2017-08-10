43e7f4be4a036a2d1022a20a1dfc91aea40d5454
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-08-10 10:24:51 +0100

    Added IOC code

---
+++ src/SFA.DAS.Tasks.API/DependancyResolution/DefaultRegistry.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using MediatR;
+using SFA.DAS.NLog.Logger;
+using StructureMap;
+
+namespace SFA.DAS.Tasks.API.DependancyResolution
+{
+    public class DefaultRegistry : Registry
+    {
+        //private const string ServiceName = "SFA.DAS.Tasks.API";
+        private const string ServiceNamespace = "SFA.DAS.Tasks";
+
+        public DefaultRegistry()
+        {
+            Scan(
+                scan =>
+                {
+                    scan.AssembliesFromApplicationBaseDirectory(a => a.GetName().Name.StartsWith(ServiceNamespace));
+                    scan.RegisterConcreteTypesAgainstTheFirstInterface();
+                });
+         
+            RegisterMediator();
+            RegisterLogger();
+        }
+
+        private void RegisterLogger()
+        {
+            For<IRequestContext>().Use(x => new RequestContext(new HttpContextWrapper(HttpContext.Current)));
+            For<ILog>().Use(x => new NLogLogger(
+                x.ParentType,
+                x.GetInstance<IRequestContext>(),
+                null)).AlwaysUnique();
+        }
+
+        private void RegisterMediator()
+        {
+            For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+            For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+            For<IMediator>().Use<Mediator>();
+        }
+    }
+}
