d3ea757a8cc55c3ff5d51266bc73318537111472
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-10-23 14:54:44 +0100

    Implemented the remove legal entity processor
    - This fixes the gap on the sign agreement tasks where a organisation is removed before the related agreement is signed and the task counter is not decremented

---
+++ src/SFA.DAS.Tasks.Worker/MessageProcessors/LegalEntityRemovedMessageProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerAccounts.Events.Messages;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.AzureServiceBus.Attributes;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.Application.Commands.SaveTask;
+using SFA.DAS.Tasks.API.Types.Enums;
+
+namespace SFA.DAS.Tasks.Worker.MessageProcessors
+{
+    [TopicSubscription("Task_LegalEntityRemovedProcessor")]
+    public class LegalEntityRemovedMessageProcessor : MessageProcessor<LegalEntityRemovedMessage>
+    {
+        private readonly ILog _logger;
+        private readonly IMediator _mediator;
+
+        public LegalEntityRemovedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog logger, IMediator mediator) : base(subscriberFactory, logger)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        protected override async Task ProcessMessage(LegalEntityRemovedMessage message)
+        {
+            _logger.Debug($"Legal entity is being removed (ID:{message?.LegalEntityId})");
+
+            if (message?.AgreementSigned != null && !message.AgreementSigned)
+            {
+                _logger.Debug("Legal entity's agreement task is also being removed as it is incomplete");
+                await _mediator.SendAsync(new SaveTaskCommand
+                {
+                    OwnerId = message.AccountId.ToString(),
+                    Type = TaskType.AgreementToSign,
+                    TaskCompleted = true
+                });
+            }
+        }
+    }
+}
