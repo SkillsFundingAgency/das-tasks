b274d8594300526650fa0a037816ea1b8b5eab57
Robin O'Connor <github@bytepointsoftware.com>
2017-10-31 14:45:11 +0000

    added message processor for apprentice change created

---
+++ src/SFA.DAS.Tasks.Worker.UnitTests/MessageProcessors/ApprenticeUpdateCreatedMessageProcessorTests/WhenAApprenticeshipUpdateCreatedMessageIsProcessed.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Events;
+using SFA.DAS.EmployerAccounts.Events.Messages;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.Application.Commands.SaveTask;
+using SFA.DAS.Tasks.API.Types.Enums;
+using SFA.DAS.Tasks.Worker.MessageProcessors;
+
+namespace SFA.DAS.Tasks.Worker.UnitTests.MessageProcessors.ApprenticeUpdateCreatedMessageProcessorTests
+{
+    public class WhenAApprenticeshipUpdateCreatedMessageIsProcessed
+    {
+        private Mock<IMessageSubscriberFactory> _subscriptionFactory;
+        private Mock<IMessageSubscriber<ApprenticeshipUpdateCreated>> _subscriber;
+        private ApprenticeshipUpdateCreated _messageContent;
+        private Mock<IMessage<ApprenticeshipUpdateCreated>> _mockMessage;
+        private Mock<IMediator> _mediator;
+        private CancellationTokenSource _tokenSource;
+        private ApprenticeUpdateCreatedMessageProcessor _processor;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _subscriptionFactory = new Mock<IMessageSubscriberFactory>();
+            _subscriber = new Mock<IMessageSubscriber<ApprenticeshipUpdateCreated>>();
+
+            _messageContent = new ApprenticeshipUpdateCreated();
+
+            _mockMessage = new Mock<IMessage<ApprenticeshipUpdateCreated>>();
+
+            _mockMessage.Setup(x => x.Content).Returns(_messageContent);
+
+            _mediator = new Mock<IMediator>();
+            _tokenSource = new CancellationTokenSource();
+
+            _processor = new ApprenticeUpdateCreatedMessageProcessor(_subscriptionFactory.Object, Mock.Of<ILog>(),
+                _mediator.Object);
+
+            _subscriptionFactory.Setup(x => x.GetSubscriber<ApprenticeshipUpdateCreated>()).Returns(_subscriber.Object);
+
+            _subscriber.Setup(x => x.ReceiveAsAsync())
+                .ReturnsAsync(() => _mockMessage.Object)
+                .Callback(() => { _tokenSource.Cancel(); });
+        }
+
+        [Test]
+        public async Task ThenTheMessageShouldBeHandledByAHandler()
+        {
+            //Act
+            await _processor.RunAsync(_tokenSource.Token);
+
+            //Assert
+            _mediator.Verify(x => x.SendAsync(It.Is<SaveTaskCommand>(cmd => cmd.OwnerId.Equals(_messageContent.AccountId.ToString()) &&
+                                                                            cmd.Type.Equals(TaskType.ApprenticeChangesToReview) &&
+                                                                            cmd.TaskCompleted.Equals(false))), Times.Once);
+        }
+    }
+}
---
+++ src/SFA.DAS.Tasks.Worker/MessageProcessors/ApprenticeUpdateCreatedMessageProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Commitments.Events;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.Application.Commands.SaveTask;
+using SFA.DAS.Tasks.API.Types.Enums;
+
+namespace SFA.DAS.Tasks.Worker.MessageProcessors
+{
+    public class ApprenticeUpdateCreatedMessageProcessor : MessageProcessor<ApprenticeshipUpdateCreated>
+    {
+        private readonly ILog _log;
+        private readonly IMediator _mediator;
+
+        public ApprenticeUpdateCreatedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog log, IMediator mediator) : base(subscriberFactory, log)
+        {
+            _log = log;
+            _mediator = mediator;
+        }
+
+        protected override async Task ProcessMessage(ApprenticeshipUpdateCreated message)
+        {
+            _log.Debug($"Saving 'apprentice changes to review' task for account id {message.AccountId}, " +
+                          $"apprentice id {message.ApprenticeshipId} and provider id {message.ProviderId}");
+
+            await _mediator.SendAsync(new SaveTaskCommand
+            {
+                OwnerId = message.AccountId.ToString(),
+                Type = TaskType.ApprenticeChangesToReview,
+                TaskCompleted = false
+            });
+        }
+    }
+}
