1eb241d7410d002bc739d949e2468d93417d1edb
Robin O'Connor <github@bytepointsoftware.com>
2017-10-31 14:48:51 +0000

    Added subscriber attribute

--- src/SFA.DAS.Tasks.Worker/MessageProcessors/ApprenticeUpdateCreatedMessageProcessor.cs
+++ src/SFA.DAS.Tasks.Worker/MessageProcessors/ApprenticeUpdateCreatedMessageProcessor.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using SFA.DAS.Commitments.Events;
 using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.AzureServiceBus.Attributes;
 using SFA.DAS.Messaging.Interfaces;
 using SFA.DAS.NLog.Logger;
 using SFA.DAS.Tasks.Application.Commands.SaveTask;
@@ -9,12 +10,14 @@ using SFA.DAS.Tasks.API.Types.Enums;
 
 namespace SFA.DAS.Tasks.Worker.MessageProcessors
 {
+    [TopicSubscription("Task_ApprenticeUpdateCreatedMessageProcessor")]
     public class ApprenticeUpdateCreatedMessageProcessor : MessageProcessor<ApprenticeshipUpdateCreated>
     {
         private readonly ILog _log;
         private readonly IMediator _mediator;
 
-        public ApprenticeUpdateCreatedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog log, IMediator mediator) : base(subscriberFactory, log)
+        public ApprenticeUpdateCreatedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog log, IMediator mediator) 
+            : base(subscriberFactory, log)
         {
             _log = log;
             _mediator = mediator;
