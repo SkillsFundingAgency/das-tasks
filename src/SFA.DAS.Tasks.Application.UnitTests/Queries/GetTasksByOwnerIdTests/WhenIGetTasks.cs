55ee824c90378d188c9d7fcf527312e0c4f6fa45
Robin O'Connor <github@bytepointsoftware.com>
2017-11-03 13:08:48 +0000

    Added monthly reminder tasks to Query
    - Now we return both owner tasks and monthly reminder ones together

--- src/SFA.DAS.Tasks.Application.UnitTests/Queries/GetTasksByOwnerIdTests/WhenIGetTasks.cs
+++ src/SFA.DAS.Tasks.Application.UnitTests/Queries/GetTasksByOwnerIdTests/WhenIGetTasks.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Tasks.Application.Queries.GetTasksByOwnerId;
 using SFA.DAS.Tasks.Application.Validation;
+using SFA.DAS.Tasks.API.Types.Enums;
+using SFA.DAS.Tasks.Domain.Models;
 using SFA.DAS.Tasks.Domain.Repositories;
 
 namespace SFA.DAS.Tasks.Application.UnitTests.Queries.GetTasksByOwnerIdTests
@@ -13,30 +16,43 @@ namespace SFA.DAS.Tasks.Application.UnitTests.Queries.GetTasksByOwnerIdTests
         private const string TaskOwnerId = "123ACX";
 
         private Mock<ITaskRepository> _repository;
-        private List<Domain.Models.DasTask> _tasks;
+        private List<DasTask> _tasks;
+        private List<DasTask> _monthlyRemindertasks;
 
         public override GetTasksByOwnerIdRequest Query { get; set; }
         public override GetTasksByOwnerIdHandler RequestHandler { get; set; }
         public override Mock<IValidator<GetTasksByOwnerIdRequest>> RequestValidator { get; set; }
-        
 
         [SetUp]
         public void Arrange()
         {
             base.SetUp();
 
-            _tasks = new List<Domain.Models.DasTask>
+            _tasks = new List<DasTask>
             {
-                new Domain.Models.DasTask()
+                new DasTask
+                {
+                    Type = TaskType.AgreementToSign
+                }
+            };
+
+            _monthlyRemindertasks = new List<DasTask>
+            {
+                new DasTask
+                {
+                    Type = TaskType.LevyDeclarationDue
+                }
             };
 
             _repository = new Mock<ITaskRepository>();
             _repository.Setup(x => x.GetTasks(It.IsAny<string>())).ReturnsAsync(_tasks);
-            
+            _repository.Setup(x => x.GetMonthlyReminderTasks()).ReturnsAsync(_monthlyRemindertasks);
+
             RequestHandler = new GetTasksByOwnerIdHandler(_repository.Object, RequestValidator.Object);
             Query = new GetTasksByOwnerIdRequest{ OwnerId = TaskOwnerId};
         }
        
+        [Test]
         public override async Task ThenIfTheMessageIsValidTheRepositoryIsCalled()
         {
             //Act
@@ -44,15 +60,20 @@ namespace SFA.DAS.Tasks.Application.UnitTests.Queries.GetTasksByOwnerIdTests
 
             //Assert
             _repository.Verify(x => x.GetTasks(TaskOwnerId), Times.Once);
+            _repository.Verify(x => x.GetMonthlyReminderTasks(), Times.Once);
         }
 
+        [Test]
         public override async Task ThenIfTheMessageIsValidTheValueIsReturnedInTheResponse()
         {
+            //Arrange
+            var expectedTasks = _tasks.Concat(_monthlyRemindertasks).ToList();
+
             //Act
             var result = await RequestHandler.Handle(Query);
 
             //Assert
-            Assert.AreEqual(_tasks, result.Tasks);
+            Assert.AreEqual(expectedTasks, result.Tasks);
         }
     }
 }
