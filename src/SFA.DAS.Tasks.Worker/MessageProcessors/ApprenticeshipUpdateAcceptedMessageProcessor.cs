ec8adc12b4755b4cb1f14c1eb3eb713c00b0f874
Robin O'Connor <github@bytepointsoftware.com>
2017-12-04 13:55:22 +0000

    Added more processor to handle previously unsupported messages

---
+++ src/SFA.DAS.Tasks.Worker/MessageProcessors/ApprenticeshipUpdateAcceptedMessageProcessor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Commitments.Events;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.Application.Commands.SaveTask;
+using SFA.DAS.Tasks.API.Types.Enums;
+
+namespace SFA.DAS.Tasks.Worker.MessageProcessors
+{
+    public class ApprenticeshipUpdateAcceptedMessageProcessor : MessageProcessor<ApprenticeshipUpdateAccepted>
+    {
+        private readonly ILog _logger;
+        private readonly IMediator _mediator;
+
+        public ApprenticeshipUpdateAcceptedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog logger, IMediator mediator) 
+            : base(subscriberFactory, logger)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        protected override async Task ProcessMessage(ApprenticeshipUpdateAccepted message)
+        {
+            _logger.Debug($"Completing 'apprentice changes to review' task for account id {message.AccountId}, " +
+                       $"apprentice id {message.ApprenticeshipId} and provider id {message.ProviderId}");
+
+            await _mediator.SendAsync(new SaveTaskCommand
+            {
+                OwnerId = message.AccountId.ToString(),
+                Type = TaskType.ApprenticeChangesToReview,
+                TaskCompleted = true
+            });
+        }
+    }
+}
---
+++ src/SFA.DAS.Tasks.Worker/MessageProcessors/CohortCreatedMessageProcessor.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.AzureServiceBus.Attributes;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.Application.Commands.SaveTask;
+using SFA.DAS.Tasks.API.Types.Enums;
+
+namespace SFA.DAS.Tasks.Worker.MessageProcessors
+{
+    [TopicSubscription("Task_CohortCreatedMessageProcessor")]
+    public class CohortCreatedMessageProcessor : MessageProcessor<CohortCreated>
+    {
+        private readonly ILog _logger;
+        private readonly IMediator _mediator;
+
+        public CohortCreatedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog log, IMediator mediator) 
+            : base(subscriberFactory, log)
+        {
+            _logger = log;
+            _mediator = mediator;
+        }
+
+        protected override async Task ProcessMessage(CohortCreated message)
+        {
+            _logger.Debug($"Completing add aprrentices task for account id {message.AccountId}");
+
+            await _mediator.SendAsync(new SaveTaskCommand
+            {
+                OwnerId = message.AccountId.ToString(),
+                Type = TaskType.AddApprentices,
+                TaskCompleted = true
+            });
+        }
+    }
+
+
+    public class CohortCreated
+    {
+        //Needs a parameterless constructor to work with the message processing
+        public CohortCreated()
+        {
+
+        }
+
+        public CohortCreated(long accountId, long? providerId, long commitmentId)
+        {
+            AccountId = accountId;
+            ProviderId = providerId;
+            CommitmentId = commitmentId;
+        }
+
+        public long AccountId { get; private set; }
+        public long? ProviderId { get; private set; }
+        public long CommitmentId { get; private set; }
+    }
+}
