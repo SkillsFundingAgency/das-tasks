55b65120d40f84965649d388064aa28f583f72aa
Robin O'Connor <github@bytepointsoftware.com>
2018-01-10 11:27:27 +0000

    Updated message packages and added new commintments message processor
    - Now we consume the ProviderCohortApprovalUndoneByEmployerUpdate Message

--- src/SFA.DAS.Tasks.Worker/MessageProcessors/ProviderCohortApprovalUndoneByEmployerUpdateMessageProcessor.cs
+++ src/SFA.DAS.Tasks.Worker/MessageProcessors/ProviderCohortApprovalUndoneByEmployerUpdateMessageProcessor.cs
@@ -1,36 +1,32 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.Commitments.Events;
 using SFA.DAS.Messaging;
-using SFA.DAS.Messaging.AzureServiceBus.Attributes;
 using SFA.DAS.Messaging.Interfaces;
 using SFA.DAS.NLog.Logger;
 using SFA.DAS.Tasks.Application.Commands.SaveTask;
 using SFA.DAS.Tasks.API.Types.Enums;
-using SFA.DAS.Tasks.Infrastructure.Attributes;
 
 namespace SFA.DAS.Tasks.Worker.MessageProcessors
 {
-    [ServiceBusConnectionString("Commitments")]
-    [TopicSubscription("Task_ApprovedCohortReturnedToProvider")]
-    public class ApprovedCohortReturnedToProviderMessageProcessor : MessageProcessor<ApprovedCohortReturnedToProvider>
+    public class ProviderCohortApprovalUndoneByEmployerUpdateMessageProcessor : MessageProcessor<ProviderCohortApprovalUndoneByEmployerUpdate>
     {
         private readonly ILog _logger;
         private readonly IMediator _mediator;
 
-        public ApprovedCohortReturnedToProviderMessageProcessor(
+        public ProviderCohortApprovalUndoneByEmployerUpdateMessageProcessor(
             IMessageSubscriberFactory subscriberFactory, 
-            ILog logger, 
-            IMediator mediator) 
-            : base(subscriberFactory, logger)
+            ILog logger,
+            IMediator mediator) : base(subscriberFactory, logger)
         {
             _logger = logger;
             _mediator = mediator;
         }
 
-        protected override async Task ProcessMessage(ApprovedCohortReturnedToProvider message)
+        protected override async Task ProcessMessage(ProviderCohortApprovalUndoneByEmployerUpdate message)
         {
-            _logger.Debug($"Cohort Approval sent back to provider. Completing 'cohort approval requested' task for account id {message.AccountId}, " +
+            _logger.Debug($"Employer updated cohort, provider approval undone by change. Completing 'cohort approval requested' task for account id {message.AccountId}, " +
                           $"commitment id {message.CommitmentId} and provider id {message.ProviderId}");
 
             await _mediator.SendAsync(new SaveTaskCommand
