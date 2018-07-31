c2df1dc8c81bd83ce16874727f5e5a42df509772
Ian Gledhill <ian@gledhills.mobi>
2018-07-31 08:46:11 +0100

    Add message processors for Transfer Request task

---
+++ src/SFA.DAS.Tasks.Worker/MessageProcessors/CohortApprovalRequestedBySenderMessageProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Commitments.Events;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.AzureServiceBus.Attributes;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.Application.Commands.SaveTask;
+using SFA.DAS.Tasks.API.Types.Enums;
+using SFA.DAS.Tasks.Infrastructure.Attributes;
+
+namespace SFA.DAS.Tasks.Worker.MessageProcessors
+{
+    [ServiceBusConnectionString("Commitments")]
+    [TopicSubscription("Task_CohortApprovalByTransferSenderRequested")]
+    public class CohortApprovalRequestedBySenderMessageProcessor : MessageProcessor<CohortApprovalByTransferSenderRequested>
+    {
+        private readonly ILog _log;
+        private readonly IMediator _mediator;
+
+        public CohortApprovalRequestedBySenderMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog log, IMediator mediator) 
+            : base(subscriberFactory, log)
+        {
+            _log = log;
+            _mediator = mediator;
+        }
+
+        protected override async Task ProcessMessage(CohortApprovalByTransferSenderRequested message)
+        {
+            _log.Debug($"Cohort approval requested by sender. Saving 'TransferRequestReceived' task for account id {message.SendingEmployerAccountId}, " +
+                       $"commitment id {message.CommitmentId}");
+
+            await _mediator.SendAsync(new SaveTaskCommand
+            {
+                EmployerAccountId = message.SendingEmployerAccountId.ToString(),
+                Type = TaskType.TransferRequestReceived,
+                TaskCompleted = false
+            });
+        }
+    }
+}
---
+++ src/SFA.DAS.Tasks.Worker/MessageProcessors/CohortApprovedBySenderMessageProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Commitments.Events;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.AzureServiceBus.Attributes;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.Application.Commands.SaveTask;
+using SFA.DAS.Tasks.API.Types.Enums;
+using SFA.DAS.Tasks.Infrastructure.Attributes;
+
+namespace SFA.DAS.Tasks.Worker.MessageProcessors
+{
+    [ServiceBusConnectionString("Commitments")]
+    [TopicSubscription("Task_CohortApprovedByTransferSender")]
+    public class CohortApprovedBySenderMessageProcessor : MessageProcessor<CohortApprovedByTransferSender>
+    {
+        private readonly ILog _log;
+        private readonly IMediator _mediator;
+
+        public CohortApprovedBySenderMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog log, IMediator mediator) 
+            : base(subscriberFactory, log)
+        {
+            _log = log;
+            _mediator = mediator;
+        }
+
+        protected override async Task ProcessMessage(CohortApprovedByTransferSender message)
+        {
+            _log.Debug($"Cohort approved by sender. Saving 'TransferRequestReceived' task for account id {message.SendingEmployerAccountId}, " +
+                       $"commitment id {message.CommitmentId}");
+
+            await _mediator.SendAsync(new SaveTaskCommand
+            {
+                EmployerAccountId = message.SendingEmployerAccountId.ToString(),
+                Type = TaskType.TransferRequestReceived,
+                TaskCompleted = true
+            });
+        }
+    }
+}
