65d06e136b2429b39b9dba8f6e796c605249f562
chrisfoster76 <chrisfoster186@googlemail.com>
2021-12-13 10:13:54 +0000

    Use MI if no SharedAccessKey is present in connection string

--- src/SFA.DAS.Tasks.Infrastructure/DependencyResolution/Configuration/MessageSubscriberPolicy.cs
+++ src/SFA.DAS.Tasks.Infrastructure/DependencyResolution/Configuration/MessageSubscriberPolicy.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Configuration;
 using System.Linq;
 using System.Reflection;
-using Microsoft.Azure;
-using SFA.DAS.Configuration;
-using SFA.DAS.Configuration.AzureTableStorage;
-using SFA.DAS.Configuration.FileStorage;
 using SFA.DAS.Messaging.AzureServiceBus;
 using SFA.DAS.Messaging.AzureServiceBus.Helpers;
 using SFA.DAS.Messaging.FileSystem;
 using SFA.DAS.Messaging.Interfaces;
 using SFA.DAS.NLog.Logger;
 using SFA.DAS.Tasks.Domain.Configurations;
-using StructureMap;
 using StructureMap.Pipeline;
 
 namespace SFA.DAS.Tasks.Infrastructure.DependencyResolution.Configuration 
@@ -46,7 +40,9 @@ namespace SFA.DAS.Tasks.Infrastructure.DependencyResolution.Configuration
             {
                 var subscriptionName = TopicSubscriptionHelper.GetMessageGroupName(instance.Constructor.DeclaringType);
 
-                var factory = new TopicSubscriberFactory(messageQueueConnectionString, subscriptionName, new NLogLogger(typeof(TopicSubscriberFactory)));
+                var useManagedIdentity = !messageQueueConnectionString.Contains("SharedAccessKey");
+
+                var factory = new TopicSubscriberFactory(messageQueueConnectionString, subscriptionName, new NLogLogger(typeof(TopicSubscriberFactory)), false, useManagedIdentity);
 
                 instance.Dependencies.AddForConstructorParameter(subscriberFactory, factory);
             }   
