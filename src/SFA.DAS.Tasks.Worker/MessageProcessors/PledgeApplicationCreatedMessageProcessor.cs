6331c91950037f9db24d5900147a53073aa00371
chrisfoster76 <chrisfoster186@googlemail.com>
2021-12-15 12:40:50 +0000

    Adds processor

---
+++ src/SFA.DAS.Tasks.Worker/MessageProcessors/PledgeApplicationCreatedMessageProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.LevyTransferMatching.Messages.Legacy;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.AzureServiceBus.Attributes;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.API.Types.Enums;
+using SFA.DAS.Tasks.Application.Commands.SaveTask;
+using SFA.DAS.Tasks.Infrastructure.Attributes;
+
+namespace SFA.DAS.Tasks.Worker.MessageProcessors
+{
+    [ServiceBusConnectionString("SharedServicesBus")]
+    [TopicSubscription("Task_PledgeApplicationCreated")]
+    public class PledgeApplicationCreatedMessageProcessor : MessageProcessor<PledgeApplicationCreated>
+    {
+        private readonly ILog _logger;
+        private readonly IMediator _mediator;
+
+        public PledgeApplicationCreatedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog logger, IMediator mediator)
+            : base(subscriberFactory, logger)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        protected override async Task ProcessMessage(PledgeApplicationCreated message)
+        {
+            _logger.Debug($"PledgeApplicationCreated id {message.ApplicationId}");
+
+            await _mediator.SendAsync(new SaveTaskCommand
+            {
+                EmployerAccountId = message.TransferSenderId.ToString(),
+                Type = TaskType.PledgeApplicationForReview,
+                TaskCompleted = false
+            });
+        }
+    }
+}
