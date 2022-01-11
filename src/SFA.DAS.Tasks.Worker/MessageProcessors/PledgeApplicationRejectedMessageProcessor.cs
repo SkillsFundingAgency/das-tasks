43f3b7cca582e8daf603c737444d830c4c5e7bc5
chrisfoster76 <chrisfoster186@googlemail.com>
2022-01-11 15:15:32 +0000

    Adds processors back in

--- src/SFA.DAS.Tasks.Worker/MessageProcessors/PledgeApplicationRejectedMessageProcessor.cs
+++ src/SFA.DAS.Tasks.Worker/MessageProcessors/PledgeApplicationRejectedMessageProcessor.cs
@@ -1,40 +1,40 @@
-﻿//using System.Threading.Tasks;
-//using MediatR;
-//using SFA.DAS.LevyTransferMatching.Messages.Legacy;
-//using SFA.DAS.Messaging;
-//using SFA.DAS.Messaging.AzureServiceBus.Attributes;
-//using SFA.DAS.Messaging.Interfaces;
-//using SFA.DAS.NLog.Logger;
-//using SFA.DAS.Tasks.API.Types.Enums;
-//using SFA.DAS.Tasks.Application.Commands.SaveTask;
-//using SFA.DAS.Tasks.Infrastructure.Attributes;
+﻿using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.LevyTransferMatching.Messages.Legacy;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.AzureServiceBus.Attributes;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.API.Types.Enums;
+using SFA.DAS.Tasks.Application.Commands.SaveTask;
+using SFA.DAS.Tasks.Infrastructure.Attributes;
 
-//namespace SFA.DAS.Tasks.Worker.MessageProcessors
-//{
-//    [ServiceBusConnectionString("SharedServiceBus")]
-//    [TopicSubscription("Task_PledgeApplicationRejected")]
-//    public class PledgeApplicationRejectedMessageProcessor : MessageProcessor<PledgeApplicationRejected>
-//    {
-//        private readonly ILog _logger;
-//        private readonly IMediator _mediator;
+namespace SFA.DAS.Tasks.Worker.MessageProcessors
+{
+    [ServiceBusConnectionString("SharedServiceBus")]
+    [TopicSubscription("Task_PledgeApplicationRejected")]
+    public class PledgeApplicationRejectedMessageProcessor : MessageProcessor<PledgeApplicationRejected>
+    {
+        private readonly ILog _logger;
+        private readonly IMediator _mediator;
 
-//        public PledgeApplicationRejectedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog logger, IMediator mediator)
-//            : base(subscriberFactory, logger)
-//        {
-//            _logger = logger;
-//            _mediator = mediator;
-//        }
+        public PledgeApplicationRejectedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog logger, IMediator mediator)
+            : base(subscriberFactory, logger)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
 
-//        protected override async Task ProcessMessage(PledgeApplicationRejected message)
-//        {
-//            _logger.Debug($"PledgeApplicationRejected id {message.ApplicationId}");
+        protected override async Task ProcessMessage(PledgeApplicationRejected message)
+        {
+            _logger.Debug($"PledgeApplicationRejected id {message.ApplicationId}");
 
-//            await _mediator.SendAsync(new SaveTaskCommand
-//            {
-//                EmployerAccountId = message.TransferSenderId.ToString(),
-//                Type = TaskType.PledgeApplicationForReview,
-//                TaskCompleted = true
-//            });
-//        }
-//    }
-//}
+            await _mediator.SendAsync(new SaveTaskCommand
+            {
+                EmployerAccountId = message.TransferSenderId.ToString(),
+                Type = TaskType.PledgeApplicationForReview,
+                TaskCompleted = true
+            });
+        }
+    }
+}
