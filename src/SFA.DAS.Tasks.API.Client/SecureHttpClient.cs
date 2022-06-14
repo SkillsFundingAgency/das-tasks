1887dee1ab5a3a3d948f8045a2eb5bc020371b55
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2022-06-14 15:59:57 +0100

    MAC-17 - Tasks API to accept MI tokens from Clients calling the API

--- src/SFA.DAS.Tasks.API.Client/SecureHttpClient.cs
+++ src/SFA.DAS.Tasks.API.Client/SecureHttpClient.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace SFA.DAS.Tasks.API.Client
@@ -17,34 +18,32 @@ namespace SFA.DAS.Tasks.API.Client
             // So we can mock for testing
         }
 
-        private static async Task<AuthenticationResult> GetAuthenticationResult(string clientId, string appKey, string resourceId, string tenant)
+        private static async Task<string> GetAuthenticationResult(string clientId, string clientSecret, string identifierUri, string tenant)
         {
             if (string.IsNullOrEmpty(clientId) ||
-                string.IsNullOrEmpty(appKey) ||
-                string.IsNullOrEmpty(resourceId) ||
+                string.IsNullOrEmpty(clientSecret) ||
+                string.IsNullOrEmpty(identifierUri) ||
                 string.IsNullOrEmpty(tenant))
             {
                 return null;
             }
 
-            var authority = $"https://login.microsoftonline.com/{tenant}";
-            var clientCredential = new ClientCredential(clientId, appKey);
-            var context = new AuthenticationContext(authority, true);
-            var result = await context.AcquireTokenAsync(resourceId, clientCredential);
-            return result;
+            var accessToken = IsClientCredentialConfiguration(clientId, clientSecret, tenant)
+               ? await GetClientCredentialAuthenticationResult(clientId, clientSecret, identifierUri, tenant)
+               : await GetManagedIdentityAuthenticationResult(identifierUri);
+
+            return accessToken;
         }
 
         public virtual async Task<string> GetAsync(string url)
         {
-            var authenticationResult = await GetAuthenticationResult(_configuration.ClientId, _configuration.ClientSecret, _configuration.IdentifierUri, _configuration.Tenant);
+            var accessToken = await GetAuthenticationResult(_configuration.ClientId, _configuration.ClientSecret, _configuration.IdentifierUri, _configuration.Tenant);
 
             using (var client = new HttpClient())
             {
-                if(authenticationResult != null)
-                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authenticationResult.AccessToken);
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
 
-                var response = await client.GetAsync(url);
-               
+                var response = await client.GetAsync(url);               
                 response.EnsureSuccessStatusCode();
 
                 return await response.Content.ReadAsStringAsync();
@@ -53,12 +52,11 @@ namespace SFA.DAS.Tasks.API.Client
 
         public virtual async Task<string> PostAsync(string url, HttpContent content)
         {
-            var authenticationResult = await GetAuthenticationResult(_configuration.ClientId, _configuration.ClientSecret, _configuration.IdentifierUri, _configuration.Tenant);
+            var accessToken = await GetAuthenticationResult(_configuration.ClientId, _configuration.ClientSecret, _configuration.IdentifierUri, _configuration.Tenant);
 
             using (var client = new HttpClient())
             {
-                if (authenticationResult != null)
-                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authenticationResult.AccessToken);
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
 
                 var response = await client.PostAsync(url, content);
 
@@ -66,6 +64,26 @@ namespace SFA.DAS.Tasks.API.Client
 
                 return await response.Content.ReadAsStringAsync();
             }
+        }
+
+        private static bool IsClientCredentialConfiguration(string clientId, string clientSecret, string tenant)
+        {
+            return !string.IsNullOrEmpty(clientId) && !string.IsNullOrEmpty(clientSecret) && !string.IsNullOrEmpty(tenant);
+        }
+
+        private static async Task<string> GetClientCredentialAuthenticationResult(string clientId, string clientSecret, string resource, string tenant)
+        {
+            var authority = $"https://login.microsoftonline.com/{tenant}";
+            var clientCredential = new ClientCredential(clientId, clientSecret);
+            var context = new AuthenticationContext(authority, true);
+            var result = await context.AcquireTokenAsync(resource, clientCredential);
+            return result.AccessToken;
+        }
+
+        private static async Task<string> GetManagedIdentityAuthenticationResult(string resource)
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            return await azureServiceTokenProvider.GetAccessTokenAsync(resource);
         }
     }
 }
