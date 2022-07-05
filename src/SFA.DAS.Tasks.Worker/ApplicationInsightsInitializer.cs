dfabebf54a974ec193d4abae56f91d7f76058f35
Nick Graham <34914709+NickGraham101@users.noreply.github.com>
2022-07-05 11:51:23 +0100

    Dasd 7575 Mac 65 migrate to app service (#62)
    * Converted CloudService to AppService
    
    * Adds AppInsights to worker
    
    * Add ARM template for app service & other resources
    
    * Added pipeline to build and deploy app service
    
    * Added AcceptanceTest job to pipeline
    
    * Removed staging slot deployment for worker
    
    * Updated pipeline resource service connections & bumped version
    
    * Changed deployment method to zipDeploy
    
    The app is using .NET Framework so needs to be built using MSBuild and
    deployed using WedDeploy, for most similar apps this is supported using
    a task group.  This is the only app that requires an Azure Pipelines
    step template to run the deployment.
    
    * Changed pipeline trigger to master only
    Manage Accounts Ticket: MAC-65
    
    * Removed unneeded package reference for System.Data.OracleClient
    
    * Fixed name of app in TelemetryInitializer
    
    * Added pipeline stages for TEST, TEST2 & PP
    
    * Made elasticPoolName ARM param optional
    
    * All deploy stages depends on build stage

---
+++ src/SFA.DAS.Tasks.Worker/ApplicationInsightsInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace SFA.DAS.Tasks.Worker
+{
+    [ExcludeFromCodeCoverage]
+    public sealed class ApplicationInsightsInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Properties["Application"] = "SFA.DAS.Tasks.Worker";
+        }
+    }
+}
---
+++ src/SFA.DAS.Tasks.Worker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Tasks.Worker
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var workerRole = new WorkerRole();
+            workerRole.OnStart();
+            workerRole.Run();
+        }
+    }
+}
